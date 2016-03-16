d1f6fe01c46411154de596dd1683fb9bd0449413
dolewski <dolewski@student.42.fr>
2016-03-16 11:54:58 +0100

    left OK

--- srcs/event2.c
+++ srcs/event2.c
@@ -20,24 +20,23 @@ static int		calc_right(t_all *all)
 	float	tmp;
 	int		tmp2;
 
-	tmp = 0.0;
-	tmp += ((float)all->win.nb_elem/(float)all->win.nb_row);
+	tmp = 0.0 + ((float)all->win.nb_elem/(float)all->win.nb_row);
 	tmp2 = (all->win.nb_elem/all->win.nb_row);
 	i = 0;
-	stop = all->win.nb_row;
-	if (tmp > tmp2)
+	stop = 0;
+	if ((all->win.nb_elem/all->win.nb_row) > 0)
 	{
-		tmp2++;
-		i = 1;
-	}
-	if (((all->point.x / all->win.col_size) + i) == tmp2)
-	{
+		stop = all->win.nb_row;
+		if (tmp > tmp2)
+		{
+			tmp2++;
+			i = 1;
+		}
+		if (((all->point.x / all->win.col_size) + i) == tmp2)
 			stop -= (stop - (all->win.nb_elem % all->win.nb_row));
-	}
-	else if (((all->point.x / all->win.col_size) + i) == tmp2 - 1)
-	{
-		if (all->point.y >= (all->win.nb_elem % all->win.nb_row))
-			stop += (all->win.nb_elem % all->win.nb_row);
+		else if (((all->point.x / all->win.col_size) + i) == tmp2 - 1)
+			if (all->point.y >= (all->win.nb_elem % all->win.nb_row))
+				stop += (all->win.nb_elem % all->win.nb_row);
 	}
 	return (stop);
 }
@@ -58,13 +57,32 @@ int				right(t_all *all)
 	return (0);
 }
 
+static int		calc_left(t_all *all)
+{
+	int		stop;
+
+	stop = 0;
+	if ((all->win.nb_elem/all->win.nb_row) > 0)
+	{
+		stop = all->win.nb_row;
+		if (all->point.x == 0)
+		{
+			if (all->point.y >= (all->win.nb_elem % all->win.nb_row))
+				stop += (all->win.nb_elem % all->win.nb_row);
+			else
+				stop -= (stop - (all->win.nb_elem % all->win.nb_row));
+		}
+	}
+	return (stop);
+}
+
 int				left(t_all *all)
 {
 	int		i;
 	int		stop;
 
 	i = 0;
-	stop = all->win.nb_row;
+	stop = calc_left(all);
 	while (i < stop)
 	{
 		if (prev(all) == -1)
